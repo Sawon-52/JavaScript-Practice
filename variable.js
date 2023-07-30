5eb8968a57d4dde4dda6a24890ad245f85d3b3bc
Sawon-52 <mehedihasansawon.52@gmail.com>
2023-07-30 13:34:12 +0600

    added variable file

---
+++ variable.js
@@ -0,0 +1,34 @@
+//there are many type of variable 
+// data is two type 1. primmitive and 2.Non-Primitive
+
+//primitive is 
+//number
+var num =3;
+
+//string
+var name ='mehedi';
+var num ='30.20';
+//we can convert string to intiger and float
+var numConvert = parseInt(num);
+console.log(numConvert);
+
+var numFloat = parseFloat(num);
+console.log(numFloat);
+
+//boolean
+var isTrue = true;
+console.log(typeof(isTrue));
+
+var isFalse = false;
+console.log(typeof(isFalse));
+
+//undefined
+var somthng;
+console.log(typeof(somthng));
+
+//null
+var name1 = null;
+console.log(typeof(name1));
+
+
+
