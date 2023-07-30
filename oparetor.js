d64569d41ad3f6e2f89ed593541fd28019f4330a
Sawon-52 <mehedihasansawon.52@gmail.com>
2023-07-30 13:59:40 +0600

    added arithmetic oparetor

---
+++ oparetor.js
@@ -0,0 +1,45 @@
+//if 2+4 = 6 here 2 and 4 are oparend and + is oparetor 
+// so An operator, in computer programing, is a symbol that usually represents an action or process.
+
+//Arithmentic Oparetor
+//comparsion Oparetor
+//Logical /Relational Oparetor
+//Assignment Oparetor
+//Conditional / Ternary oparetor
+
+//Arithmentic Oparetor
+//addition
+var addition = 23 + 24;
+console.log(addition);
+
+//subtraction
+var subtraction = 40 - 10 ;
+console.log(subtraction);
+
+//multiplecation
+var multiplecation = 10 * 10;
+console.log(multiplecation);
+
+//division
+var division = 200 / 20;
+console.log(division);
+
+//modulas 
+var modulu = 13 % 5;
+console.log(modulu);
+
+//incrementAl a = a+1
+
+var a = 10;
+a++
+console.log(a);
+
+//decrement a = a -1;
+
+var b = 10;
+b--
+console.log(b);
+
+
+
+
