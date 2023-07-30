10ae2712f5dd9f50f979fdfbf3feb0add8d7634c
Sawon-52 <mehedihasansawon.52@gmail.com>
2023-07-30 13:47:44 +0600

    added concatenetion file

---
+++ concatenetion.js
@@ -0,0 +1,8 @@
+//we used + sign for concatinate two string for example
+
+var fristName ='Mehedi';
+var lastName =' Hasan';
+
+ var fullName = fristName + " " + lastName;
+
+ console.log(fullName);
