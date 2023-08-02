9b7cb8917f876139f41a86bd00ff5672d8b1321f
Sawon-52 <mehedihasansawon.52@gmail.com>
2023-08-02 11:47:03 +0600

    create conditional file and added conditional statement

---
+++ conditional.js
@@ -0,0 +1,59 @@
+// এখন রাত(সত্য হইলে):
+//    তাইলে ঘুমানোর টাইম
+// না এখন সকাল(সত্য হইলে):
+//    তাইলে ঘুম থেকে উঠার টাইম
+// না এখন বিকাল(সত্য হইলে):
+//    তাইলে ঘুরতে যেতে হবে
+// কোনোটাই সত্য না:
+//    তাইলে বসে থাকি
+
+// 1। if statement
+
+// 2। if...else statement
+
+// 3। if...else if statement
+// আর আপনাকে অবশ্যই মনে রাখতে হবে। আপনি if (expression) এখানে এক্সপ্রেশনে যেটা দিবেন সেটা অবশ্যই সত্য true নাইলে মিথ্যা false রেজাল্ট দিতে হবে। কন্ডিশনাল স্টেটমেন্ট শুধু এখানে সত্য true বা মিথ্যা false অ্যাক্সেপ্ট করে, অন্যকিছু করে না।
+
+// if(সত্য কিছু হয়) {
+//     তাহলে আমার ভিতরের জিনিস এক্সিকিউট হবে
+//  } else {
+//     নাইলে আমার ভিতরের জিনিস এক্সিকিউট হবে
+//  }
+
+// 2। if...else statement
+var age = 21;
+if(age > 18){
+    console.log('Now you are Adult')
+}
+else{
+    console.log('No you are not aged enough to be a adult');
+}
+
+//if...else if statement
+// if(সত্য কিছু হয়) {
+//     তাহলে আমার ভিতরের জিনিস এক্সিকিউট হবে
+//  } else if(নইলে যদি আমি সত্য হই){
+//     তাহলে আমার ভিতরের জিনিস এক্সিকিউট হবে
+//  } else if(নইলে যদি আমি সত্য হই){
+//     তাহলে আমার ভিতরের জিনিস এক্সিকিউট হবে
+//  } 
+//  ...
+//  else {
+//     নাইলে আমার ভিতরের জিনিস এক্সিকিউট হবে
+//  }
+
+var whatDate ='Sunday';
+
+if (whatDate === 'Friday'){
+    console.log('It is closed today');
+
+}else if(whatDate === 'Thursday'){
+    console.log('It is parsially closed');
+
+}else if(whatDate === 'Saturday'){
+    console.log('Some office are closed ');
+}else{
+    console.log('Normal ! Working day');
+
+}
+
