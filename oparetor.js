df5a379d6054c4fdf0689c94ea956f6e926242ce
Sawon-52 <mehedihasansawon.52@gmail.com>
2023-08-02 10:57:22 +0600

    added oparetor

--- oparetor.js
+++ oparetor.js
@@ -28,17 +28,51 @@ console.log(division);
 var modulu = 13 % 5;
 console.log(modulu);
 
-//incrementAl a = a+1
-
+//incrementAl a = a+1;
 var a = 10;
 a++
 console.log(a);
 
-//decrement a = a -1;
-
+//decrement a = a -1
 var b = 10;
 b--
 console.log(b);
+
+//comparsion Oparetor
+// == equal 
+
+var value1 = 10;
+var value2 = "10";
+var checkValue = (value1 == value2);
+console.log(checkValue);
+
+// === cannot change data type
+var value1 = 10;
+var value2 = "10";
+var checkValue = (value1 === value2);
+console.log(checkValue);
+
+// != equal not
+var value1 = 10;
+var value2 = 20;
+var checkValue = (value1 != value2);
+console.log(checkValue);
+
+// > grater than 
+var value1 = 10;
+var value2 = 20;
+var checkValue = (value1 > value2);
+console.log(checkValue);
+
+// < less than 
+var value1 = 10;
+var value2 = 20;
+var checkValue = (value1 < value2);
+console.log(checkValue);
+
+
+
+
 
 
 
