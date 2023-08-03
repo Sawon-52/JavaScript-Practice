9888adcc8c2fec3a6282851ccda5ad8970723703
Sawon-52 <mehedihasansawon.52@gmail.com>
2023-08-03 14:29:45 +0600

    added switch statement

---
+++ switch.js
@@ -0,0 +1,21 @@
+var rollNumber = 5;
+
+switch(rollNumber){
+    case 1:
+        console.log('Romzan Ali');
+        break;
+
+    case 2:
+        console.log('kuddus Ali');
+        break;
+    case 3:
+        console.log('Rohim Ali');
+        break;
+
+    case 4:
+        console.log('kulsum bibi');
+        break;
+    default:
+        console.log('Others')
+                
+}
