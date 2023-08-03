117dd73dabf1acf532937331839b189d2f0c2acc
Sawon-52 <mehedihasansawon.52@gmail.com>
2023-08-03 14:18:03 +0600

    added do .. while loop

--- loop.js
+++ loop.js
@@ -1,6 +1,5 @@
 //for loop
 //for(initialization ; condition ; increment/decriment)
-
 var num = 14;
 for(var i=0; i<=14; i++){
     if(i===5){
@@ -10,4 +9,16 @@ for(var i=0; i<=14; i++){
     console.log(i);
 }
 
+// do .......while loop
+// do{
+//     statement
+// }
+// while(condition)
+
+var a = 10;
+do{
+    a+=1;
+    console.log(a);
+}
+while(a<=20);
 
