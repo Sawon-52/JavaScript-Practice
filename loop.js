38d0420afdfc4c5030ee162299f214909fcc4c2d
Sawon-52 <mehedihasansawon.52@gmail.com>
2023-08-03 14:21:30 +0600

    aded while loop

--- loop.js
+++ loop.js
@@ -1,5 +1,6 @@
 //for loop
 //for(initialization ; condition ; increment/decriment)
+console.log('start For loop')
 var num = 14;
 for(var i=0; i<=14; i++){
     if(i===5){
@@ -15,10 +16,23 @@ for(var i=0; i<=14; i++){
 // }
 // while(condition)
 
+console.log('start do ....while loop')
 var a = 10;
 do{
     a+=1;
     console.log(a);
 }
 while(a<=20);
+
+
+//while loop
+
+// while(condition){
+
+// }
+console.log('start while loop')
+var b = 1;
+while(b<=30){
+    console.log(b+=1);
+}
 
