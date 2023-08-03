4bcfa9e0bf3438e9db0cee20f9fd1b85f4557d31
Sawon-52 <mehedihasansawon.52@gmail.com>
2023-08-03 14:13:20 +0600

    added for loop

---
+++ loop.js
@@ -0,0 +1,13 @@
+//for loop
+//for(initialization ; condition ; increment/decriment)
+
+var num = 14;
+for(var i=0; i<=14; i++){
+    if(i===5){
+        console.log('The number is five');
+        break;
+    }
+    console.log(i);
+}
+
+
