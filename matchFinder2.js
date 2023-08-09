0c6487f7c8e4eecb4a9595553e01c8eb57c560be
Sawon-52 <mehedihasansawon.52@gmail.com>
2023-08-09 12:49:15 +0600

    added matchFinder file

---
+++ matchFinder2.js
@@ -0,0 +1,20 @@
+
+function matchFinder(string1, string2) {
+
+    if( typeof string1 !== 'string' || typeof string2 !== 'string'){
+        return('Input Must be a String');
+    }
+
+    else if(string1.includes(string2)){
+        return true;
+    }
+    else 
+    return false;
+}
+
+var name1 = 'mehedi';
+var name2= 'me';
+
+
+var ans = matchFinder(name1, name2);
+console.log(ans);
