af164eb002dce733a16aa2d7d2d60cca009b7c0f
Sawon-52 <mehedihasansawon.52@gmail.com>
2023-08-09 20:44:01 +0600

    added canPay file

---
+++ canPay.js
@@ -0,0 +1,24 @@
+function canPay(changeArray, totalDue) {
+    if(changeArray.length == 0){
+        return ('Insert Some element in Array');
+    }
+    else{
+        var total =0;
+        for(let i = 0; i<changeArray.length; i++){
+            total = total + changeArray[i];
+        }
+
+        if(total >= totalDue){
+            return true;
+        }
+        else
+        return false;
+    }
+
+}
+
+const arr =[1,5,5];
+const price =10;
+
+var result = canPay(arr,price);
+console.log(result);
