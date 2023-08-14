fe22d7ef874a73dc47e4908711882762e03896cd
Sawon-52 <mehedihasansawon.52@gmail.com>
2023-08-14 11:43:13 +0600

    create class and remove class using js

--- DOM/createElement.js
+++ DOM/createElement.js
@@ -1,10 +1,34 @@
+// how to create element
 var newElement = document.createElement('div');
 console.log(newElement);
 
 var paraElement = document.createElement('p');
 console.log(paraElement);
 
-paraElement.innerText = 'My name is mehedi hasan'
+// paraElement.innerText = 'My name is mehedi hasan'
 
 // or 
-paraElement.textContent = 'Hi this is mehedi hasan'
+paraElement.textContent = 'Hi this is mehedi'
+
+// how to insert  inner html
+newElement.innerHTML ='<h2>kno ato jontrona vai </h2> <p>ami pari na soite</p> <div> <p>kno lage </p> </div> ';
+
+
+// how to show inner HTML
+// var container = document.getElementById('container')
+// var result = container.innerHTML;
+// console.log(result);
+
+
+// how to add class in element
+newElement.classList.add('box-4');
+
+// add many class 
+newElement.classList += ' name  valolage-na';
+// checked
+console.log(newElement.classList);
+
+
+// how to remove class
+newElement.classList.remove('valolage-na');
+
